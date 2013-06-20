98f83ddea92244a29e8df9630266449d7ab0a71c
Paul <paul.roth@polarmobile.com>
2013-06-19 21:48:23 -0400

    added fireball sound effect when player turns block to lava

--- src/main/java/com/paulhroth/LavaHandsListener.java
+++ src/main/java/com/paulhroth/LavaHandsListener.java
@@ -4,6 +4,8 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.Sound;
+import org.bukkit.World;
 
 public class LavaHandsListener implements Listener {
 	
@@ -17,7 +19,9 @@ public class LavaHandsListener implements Listener {
 	public void onBlockDamage(BlockDamageEvent event) {
 		Block block = event.getBlock();
 		if (plugin.enabled(event.getPlayer())) {
-            block.setTypeId(10);
+		    World world = event.getPlayer().getWorld();
+		    world.playSound(event.getPlayer().getLocation(), Sound.GHAST_FIREBALL, 10f, 1.0f);
+		    block.setTypeId(10);
 		}
 	}
 }
